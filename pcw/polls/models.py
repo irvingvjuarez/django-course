2756d7af85ed7c3a8122a20876a41ce409fcccca
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-21 21:32:07 -0600

    Models created for the polls app

--- pcw/polls/models.py
+++ pcw/polls/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from django.utils import timesince, timezone
 
 # Create your models here.
 class Question(models.Model):
     content = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField("date published", default=timezone.now())
+
+    def __str__(self):
+        return self.content
+
+    def publishedDate(self):
+        return timesince.timesince(self.pub_date)
 
 class Choice(models.Model):
     content = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
