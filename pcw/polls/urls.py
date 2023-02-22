7a2e7aca276c52038add57f8dc5c454a3618fe2e
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-22 13:08:16 -0600

    url type used in the django template system

--- pcw/polls/urls.py
+++ pcw/polls/urls.py
@@ -1,9 +1,10 @@
 from .views import index, detail, vote, results
 from django.urls import path
 
+app_name = "polls"
 urlpatterns = [
     path('', index, name="index"),
-    path('<int:question_id>/', detail, name="question detail"),
-    path('<int:question_id>/results', results, name="question vote page"),
-    path('<int:question_id>/vote', vote, name="question results"),
+    path('<int:question_id>/', detail, name="detail"),
+    path('<int:question_id>/results', results, name="results"),
+    path('<int:question_id>/vote', vote, name="vote"),
 ]
