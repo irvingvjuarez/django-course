42003cf036b38ae1547c0e00f389ddca030963fd
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-28 11:01:00 -0600

    was_published_recently method created in the Question model

--- pcw/polls/models.py
+++ pcw/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timesince, timezone
+import datetime
 
 # Create your models here.
 class Question(models.Model):
@@ -11,6 +12,9 @@ class Question(models.Model):
 
     def publishedDate(self):
         return timesince.timesince(self.pub_date)
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     content = models.CharField(max_length=100)
